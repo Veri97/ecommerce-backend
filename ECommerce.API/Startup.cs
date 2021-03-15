2da2b7d59bdd3f329697813ada4fbfa1265bf97f
Veri97 <veri.kazazi1997@hotmail.com>
2021-03-15 21:04:18 +0100

    cleaning up startup.cs class by adding extension methods for services and middleware

--- ECommerce.API/Startup.cs
+++ ECommerce.API/Startup.cs
@@ -1,23 +1,12 @@
-using ECommerce.API.Errors;
+using ECommerce.API.Extensions;
 using ECommerce.API.Helpers;
 using ECommerce.API.Middleware;
-using ECommerce.Core.Interfaces;
 using ECommerce.Infrastructure.Data;
-using ECommerce.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ECommerce.API
 {
@@ -32,48 +21,23 @@ namespace ECommerce.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository,ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
-
+           
             services.AddAutoMapper(typeof(MappingProfiles));
 
             services.AddControllers();
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ECommerce.API", Version = "v1" });
-            });
+          
             services.AddDbContext<StoreContext>(x => x.UseSqlite(
                             _config.GetConnectionString("DefaultConnection")));
 
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.InvalidModelStateResponseFactory = actionContext =>
-                {
-                    var errors = actionContext.ModelState
-                                  .Where(e => e.Value.Errors.Count > 0)
-                                  .SelectMany(x => x.Value.Errors)
-                                  .Select(x => x.ErrorMessage)
-                                  .ToArray();
-
-                    var errorResponse = new ApiValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerce.API v1"));
-            
 
             //when a request comes in and the api does not have an endpoint to match, this middleware will redirect 
             //to EndpointNotFoundControlle, where ApiResponse is returned for this case
@@ -86,6 +50,8 @@ namespace ECommerce.API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
